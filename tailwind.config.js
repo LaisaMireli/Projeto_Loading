3432503f2f1dd02a3a8bafd4631f51cb19c5e807
LaisaMireli <laisamireli57@alu.ufc.br>
2024-05-27 11:12:34 -0300

    Projeto Final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    
     "./src/**/*.{html,js}"
   ],
+  darkMode: 'class',
   safelist: [
     { pattern: /^.*$/ },
   ],
